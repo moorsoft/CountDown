908b1ebd34dfe66df7e5ac02d6c000db25e178d6
Paul Moorcroft <paul@moorcroftsoftware.com.au>
2020-06-06 21:18:33 +1000

    Add a Version number to assembly info

--- CountDown/Properties/AssemblyInfo.cs
+++ CountDown/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CountDown")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Meeting count down timer")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Moorcroft Software")]
 [assembly: AssemblyProduct("CountDown")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © Moorcroft Software 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
